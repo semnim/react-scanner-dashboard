182ce4737359e6edd9d508429fdd25c3fea276f4
Semjon Nirmann <semjon.nirmann@yahoo.com>
2024-03-29 17:11:58 +0100

    Delete unused styles, style landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,20 @@
-import {Container, Stack, Text, Title} from "@mantine/core";
+import {Container, Group, Stack, Text, Title} from "@mantine/core";
 import {Link} from "react-router-dom";
 import {ReportUpload} from "components/report-upload/ReportUpload.tsx";
 
 export const LandingPage = () => {
   return <Stack align={"center"} gap={"md"}>
-    <Title order={1}>Your React Project At a Glance</Title>
+    <Group>
+      <Title order={1}>Your</Title>
+      <Title order={1} fw={900} style={{
+        background: "linear-gradient(180deg, black, #61DBFB)",
+        WebkitBackgroundClip: "text",
+        WebkitTextFillColor: "transparent",
+      }}
+      >React</Title>
+      <Title order={1}>Project At a Glance</Title>
+    </Group>
+
     <Text size="md" display={"flex"} style={{alignItems: "center"}}>
       Visualize your <Link to={"http://react.dev"}><img
         style={{width: "2rem", height: "2rem", marginInline: ".5rem"}}
